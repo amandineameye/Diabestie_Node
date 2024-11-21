7330ef066c0e932b669c3da72f02dbe427875dcf
Amandine Ameye <amandineameye@gmail.com>
2024-11-21 17:03:48 +0100

    Optimised authController

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,46 +2,42 @@ const jwtTool = require("../tools/jwt.tool");
 const mongoClient = require("../tools/db.tool");
 
 const authController = {
+	login: async (request, response) => {
+		const { username, password } = request.body;
+		try {
+			await mongoClient.connect();
+			const database = mongoClient.db("diabestieDB");
+			const usersData = database.collection("usersData");
+			const options = {
+				projection: { password: 1, _id: 0 },
+			};
+			const query = { username: username };
+			const userObject = await usersData.findOne(query, options);
 
-    login: async (request, response) => {
+			//  Ex: {password: 'myPassword98'}
 
-        const { username, password } = request.body;
-        try {
-            await mongoClient.connect();
-            const database = mongoClient.db("diabestieDB");
-            const usersData = database.collection("usersData");
-            const options = {
-                projection: {},
-            };
-            const query = {};
-            const usersDataArray = await usersData.find(query, options).toArray();
-    
-            const userObject = usersDataArray.find((user) => {
-                return user.username === username;
-            });
-    
-            if (!userObject) {
-                response.status(400).json({ error: "Username not found" });
-                return;
-            }
-    
-            if (userObject.password !== password) {
-                response.status(400).json({ error: "Wrong password" });
-                return;
-            }
-    
-            const data = {
-                username: username,
-            };
-    
-            const token = jwtTool.generate(data);
-    
-            response.status(200).json({ token: token });
-        } catch (error) {
-            console.log(error);
-        }
+			if (!userObject) {
+				response.status(400).json({ error: "Username not found" });
+				return;
+			}
 
-    }
-}
+			if (userObject.password !== password) {
+				response.status(400).json({ error: "Wrong password" });
+				return;
+			}
+
+			const data = {
+				username: username,
+			};
+
+			const token = jwtTool.generate(data);
+
+			response.status(200).json({ token: token, userObject: userObject });
+		} catch (error) {
+			console.log(error);
+			response.status(500).json({ error: "Internal Server Error" });
+		}
+	},
+};
 
 module.exports = authController;
