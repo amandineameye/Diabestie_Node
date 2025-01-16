171948d929dc95932f2a21020738596c0adc034e
Amandine <amandineameye@gmail.com>
2025-01-16 11:43:03 +0100

    Tentative 3

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(
 		origin: "http://localhost:5173",
 		methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], // Allowed HTTP methods
 		credentials: true, // Allow cookies if needed
-		allowedHeaders: ['Content-Type', 'Authorization']
+		allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin']
 	})
 );
 
