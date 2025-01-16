b29a355a5f9cd788ca00a0afa1e158b27830ff0a
Amandine <amandineameye@gmail.com>
2025-01-16 14:54:18 +0100

    vercel.json update 43

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -3,10 +3,6 @@ const authController = require("../controllers/auth.controller");
 
 const authRouter = express.Router();
 
-
-authRouter.get('/test', (req, res) => {
-    res.send(200).json({text: 'Hello World'})
-})
 authRouter.post("/auth/login", authController.login);
 authRouter.post("/auth/register", authController.register);
 
