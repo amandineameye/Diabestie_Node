4320206a169771cc516a0c22713e481cb1dfaea9
Amandine Ameye <amandineameye@gmail.com>
2025-01-19 04:08:53 +0100

    Added a / route in generalRouter

--- routes/general.route.js
+++ routes/general.route.js
@@ -4,5 +4,8 @@ const generalController = require("../controllers/general.controller");
 const generalRouter = express.Router();
 
 generalRouter.get("/general/getUserNames", generalController.getUserNames);
+generalRouter.get("/", (request, response) => {
+	return response.status(500).json({ message: "Welcome!" });
+});
 
 module.exports = generalRouter;
