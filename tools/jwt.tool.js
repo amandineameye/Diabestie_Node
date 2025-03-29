aef5c70c4533771fc4a99cfa4009d982b783f4b1
Amandine Ameye <amandineameye@gmail.com>
2025-03-29 17:46:43 +0100

    Added a README

--- tools/jwt.tool.js
+++ tools/jwt.tool.js
@@ -1,31 +1,31 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
 const jwtTool = {
-	generate: function (data) {
-		const secret = process.env.JWT_SECRET;
-		const options = {
-			algorithm: "HS512",
-			expiresIn: "1h",
-			audience: process.env.JWT_AUDIENCE,
-			issuer: process.env.JWT_ISSUER,
-		};
+  generate: function (data) {
+    const secret = process.env.JWT_SECRET;
+    const options = {
+      algorithm: 'HS512',
+      expiresIn: '1h',
+      audience: process.env.JWT_AUDIENCE,
+      issuer: process.env.JWT_ISSUER,
+    };
 
-		return jwt.sign(data, secret, options);
-	},
+    return jwt.sign(data, secret, options);
+  },
 
-	read: function (token) {
-		const secret = process.env.JWT_SECRET;
-		const options = {
-			audience: process.env.JWT_AUDIENCE,
-			issuer: process.env.JWT_ISSUER,
-		};
+  read: function (token) {
+    const secret = process.env.JWT_SECRET;
+    const options = {
+      audience: process.env.JWT_AUDIENCE,
+      issuer: process.env.JWT_ISSUER,
+    };
 
-		try {
-			return jwt.verify(token, secret, options);
-		} catch {
-			return null;
-		}
-	},
+    try {
+      return jwt.verify(token, secret, options);
+    } catch {
+      return null;
+    }
+  },
 };
 
 module.exports = jwtTool;
